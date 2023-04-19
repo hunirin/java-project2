93e645288d6dcf53fd3fe50f1009818ce9733230
hun053@naver.com <hun053@naver.com>
2023-04-19 11:33:46 +0900

    feat: Bye기능 추가 - Byeclass, ByeclassTest 추가

---
+++ src/main/java/com/java/javaproject2/Byeclass.java
@@ -0,0 +1,8 @@
+package com.java.javaproject2;
+
+public class Byeclass {
+    public void print() {
+        System.out.println("bye");
+    }
+}
+}
---
+++ src/main/java/com/java/javaproject2/ByeclassTest.java
@@ -0,0 +1,8 @@
+package com.java.javaproject2;
+
+public class ByeclassTest {
+    public static void main(String[] args) {
+        Byeclass Byeclass = new Byeclass(); // 인스턴스화
+        Byeclass.print();
+    }
+}
